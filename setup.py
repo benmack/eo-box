6501ada87d4de92dbe1e854039ec156d17e1a235
Ben Mack <ben8mack@gmail.com>
2019-02-10 13:04:05 +0100

    increasing version to follow v0.2.0 of eobox-sampledata

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ subprocess.check_call('python install_all.py', shell=True)
 
 setup(name='eo-box',
       python_requires='>=3.5,<3.7',
-      version='0.1.0',
+      version='0.2.0',
       description='A toolbox for processing earth observation data with Python.',
       long_description=get_long_description(),
       long_description_content_type='text/markdown',
