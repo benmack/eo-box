3780916bad04c6eb7f3d6aad949f9e40ff8d6727
benmack <ben8mack@gmail.com>
2019-04-21 11:44:25 +0200

    more simple internal package structure - #2

--- raster/eobox/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import raster
-from .raster import extract
-from .raster import load_extracted
-from .raster import MultiRasterIO
-
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
--- sampledata/eobox/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import sampledata
-from .sampledata import get_dataset
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
